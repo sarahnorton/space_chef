503f6602188ab82aaef9c2dc31675c367f667c63
sarahnorton <sarah.norton04@gmail.com>
2024-07-13 13:03:39 +1000

    changing object types

--- scripts/cutting_circle.gd
+++ scripts/cutting_circle.gd
@@ -1,6 +1,16 @@
 extends RigidBody2D
 
-enum State = { "cutting" }
+# dictionary to store different assets
+var cut_bar_versions = {
+	"uncut_version": preload("res://assets/Cut Bar/cut_bar_0.png"),
+	"first_cut" : preload("res://assets/Cut Bar/cut_bar_1.png"),
+	"second_cut" : preload("res://assets/Cut Bar/cut_bar_2.png"),
+	"third_cut" : preload("res://assets/Cut Bar/cut_bar_3.png"),
+	"fourth_cut" : preload("res://assets/Cut Bar/cut_bar_4.png"),
+	"successful_cut" : preload("res://assets/Cut Bar/cut_cloud.png"),
+}
+
+# enum State = { "cutting" }
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
