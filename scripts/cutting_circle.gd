035420b2afd0386b8a7339cfb64b68bfca623bd9
Fozywozy <100746142+Fozywozy@users.noreply.github.com>
2024-07-13 12:13:51 +1000

    Updated the cutting circle script
    The cutting circle will now detect when it is cut, and print the angle it was cut at.

--- scripts/cutting_circle.gd
+++ scripts/cutting_circle.gd
@@ -1,6 +1,22 @@
 extends RigidBody2D
 
-enum State = { "cutting" }
+@export var AllowedCuttingAngles = [ 1, 10 ]
+@export var AllowedVariationOnCuttingAngle = 10
+@export var NumberOfCutsToNextState = 4
+@export var NextCutScene : Resource
+
+signal ObjectCut()
+
+enum State
+{
+	Default,
+	Hovering,
+	Cutting,
+	Dragging,
+}
+
+var LocalInitialCutVector : Vector2
+var CurrentState : State = State.Default;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,13 +24,51 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
 func _process(delta):
 	pass
 
 
 func _on_control_mouse_entered():
-	pass # Replace with function body.
+	if (Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
+		LocalInitialCutVector = GetLocalPositionOfMouse()
+		CurrentState = State.Cutting
+	else:
+		CurrentState = State.Hovering
 
 
 func _on_control_mouse_exited():
-	pass # Replace with function body.
+	if (CurrentState == State.Hovering):
+		CurrentState = State.Default
+	elif (CurrentState == State.Cutting):
+		FoodCut()
+	
+	return
+
+
+func FoodCut():
+	var LocalFinalCutVector = GetLocalPositionOfMouse()
+	var DirectionVectorOfCut : Vector2 = (LocalInitialCutVector - LocalFinalCutVector).normalized()
+	var CutAngle = GetAllowedAngleDirectionVector().dot(DirectionVectorOfCut)
+	print(CutAngle)
+	
+	ObjectCut.emit()
+
+
+func GetLocalPositionOfMouse() -> Vector2:
+	var MousePosition = get_viewport().get_mouse_position()
+	
+	return to_local(MousePosition)
+
+
+func GetAllowedAngleDirectionVector() -> Vector2:
+	var Output : Vector2 = Vector2(0, 1)
+	
+	Output.rotated(AllowedCuttingAngles[0])
+	
+	return Output
+	
+
+
+
+
